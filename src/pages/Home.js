9fe1b710986cda07cb4b4c2569a538b836bdd94e
utkarsh <singhrishu680@gmail.com>
2023-02-27 22:21:00 +0530

    start app interface using module styles css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,58 @@
- const Home = () => {
-    return (
-        <div>
-        <h1>Home</h1>
+import styles from "../styles/home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.postsList}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postHeader}>
+          <div className={styles.postAvatar}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/9742/9742442.png"
+              alt="user-pic"
+            />
+            <div>
+              <span className={styles.postAuthor}>Utkarsh</span>
+              <span className={styles.postTime}>a minute ago</span>
+            </div>
+          </div>
+          <div className={styles.postContent}>Post Conetnt</div>
+
+          <div className={styles.postActions}>
+            <div className={styles.postLike}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/889/889140.png"
+                alt="likes-icon"
+              />
+              <span>5</span>
+            </div>
+
+            <div className={styles.postCommentsIcon}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/9815/9815431.png"
+                alt="comments-icon"
+              />
+              <span>2</span>
+            </div>
+          </div>
+          <div className={styles.postCommentBox}>
+            <input placeholder="Start typing a comment" />
+          </div>
+
+          <div className={styles.postCommentsList}>
+            <div className={styles.postCommentsItem}>
+              <div className={styles.postCommentHeader}>
+                <span className={styles.postCommentAuthor}>Bill</span>
+                <span className={styles.postCommentTime}>a minute ago</span>
+                <span className={styles.postCommentLikes}>22</span>
+              </div>
+
+              <div className={styles.postCommentContent}>Random comment</div>
+            </div>
+          </div>
         </div>
-    );
-    }
-    export default Home;
-   
+      </div>
+    </div>
+  );
+};
+
+export default Home;
