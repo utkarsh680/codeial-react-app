510bee13c42290952e50c21e60df1696f33a5ff5
utkarsh <singhrishu680@gmail.com>
2023-02-27 21:09:34 +0530

    adding Api

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-
+import { useEffect } from 'react';
+import { getPosts } from '../api';
+import { Home } from '../pages';
 
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log('response', response);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
-     <h1>hello world</h1>
+     <Home />
     </div>
   );
 }
